0382b7f1a6d63fb73ebf97ded3d6432d44d2e590
Jincheng Zhou <jinchenz@usc.edu>
2019-03-30 10:25:16 -0700

    LPLGraph module

---
+++ LPLGraph/LPLGraph.py
@@ -0,0 +1,12 @@
+""" Local-Prediction-Error-Learning Graph API"""
+
+
+class LPLGraph(object):
+
+    def __init__(self, state_hash_size, num_actions, num_particles=5, init_mean=0, init_std=1):
+
+        pass
+
+
+if __name__ == "__main__":
+    pass
