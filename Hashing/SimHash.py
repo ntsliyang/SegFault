0555cb2e530dac720a28451f9cf668cca007e5a7
Jincheng Zhou <jinchenz@usc.edu>
2019-04-02 17:11:45 -0700

    Create SimHash.py

---
+++ Hashing/SimHash.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+class SimHash(object):
+
+    def __init__(self, D, k, preprocessor=None):
+        """
+            Initialize SimHash function object
+        :param D: The dimension D of the raw input, or the dimension of the preprocessed input (the dimension of the
+                    output of the given preprocessor)
+        :param k: The dimension (length) of the binary hash code
+        :param preprocessor: Optional argument. A preprocessor to preprocess the given raw input
+        """
+        self._D = D
+        self._k = k
+        self._preprocessor = preprocessor
+
+    def hash(self, s, base_ten=True):
+        """
+            SimHash function
+        :param s: Raw input
+        :param base_ten: Whether to return a base-10 number of return a vector of binary values. Default to true
+        :return: Binary hashed code. Return a base-10 number if base_ten is True
+        """
+        # Preprocess input if preprocessor is given
+        if self._preprocessor is not None:
+            x = self._preprocessor(s)
+            assert x.shape == (self._D,), "The preprocessor must return a vector of shape (D,)"
+        else:
+            x = s
+        # Initialize hashing matrix
+        A = np.random.randn(self._k, self._D)
+        # Dot product
+        y = np.dot(A, x)
+        # Round to integer
+        y = (y > 0).astype(np.int0)
+        # Return output
+        out = np.asarray([y[i] * (2 ** i) for i in range(len(y))])
+        if base_ten:
+            return np.sum(out)
+        else:
+            return out
